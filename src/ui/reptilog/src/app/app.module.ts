7b7fb40320a66e1501826974c301a1cb2173d061
benjamin haas <benjamin.haas@ipacc.com>
2018-09-13 15:22:55 -0500

    lots of stuff

--- src/ui/reptilog/src/app/app.module.ts
+++ src/ui/reptilog/src/app/app.module.ts
@@ -3,13 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { PetComponent } from './components/pet/pet.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PetListComponent } from './components/pet-list/pet-list.component';
+import {AppRoutingModule} from "./app-routing.module";
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PetComponent,
+    HeaderComponent,
+    PetListComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule
   ],
   providers: [],
