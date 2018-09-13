b470a8d8b6330bf9a965407f0f8b621233d22da9
benjamin haas <benjamin.haas@ipacc.com>
2018-09-13 10:35:10 -0500

    angular project

---
+++ src/main/java/com/aws/codestar/projecttemplates/ReptilogApplication.java
@@ -0,0 +1,9 @@
+package com.aws.codestar.projecttemplates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReptilogApplication {
+    public static void main(String[] args) { SpringApplication.run(ReptilogApplication.class, args);}
+}
---
+++ src/main/java/com/aws/codestar/projecttemplates/configuration/AngularConfig.java
@@ -0,0 +1,18 @@
+//package com.aws.codestar.projecttemplates.configuration;
+//
+//import org.springframework.context.annotation.Configuration;
+//
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//@EnableWebMvc
+//public class AngularConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/").addResourceLocations("classpath:/app/");
+//    }
+//
+//}
